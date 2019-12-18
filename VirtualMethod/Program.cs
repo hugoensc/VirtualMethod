23563e589dd88ac18c95d273168ea405efbcfcc9
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2019-12-18 13:17:01 -0600

    FirstCommit

---
+++ VirtualMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
